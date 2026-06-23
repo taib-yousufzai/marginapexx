b9e9910b118c5378a36522f6c3d87b29904caeaa
Daksh-Raj00 <daksh2006rockyrony@gmail.com>
2026-06-23 17:09:21 +0530

    logo workss

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,24 +12,24 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#006400',
     icons: [
       {
-        src: '/favicon-32.png?v=7',
+        src: '/favicon-32.png?v=8',
         sizes: '32x32',
         type: 'image/png',
       },
       {
-        src: '/icon-192x192.png?v=7',
+        src: '/icon-192x192.png?v=8',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: '/icon-512x512.png?v=7',
+        src: '/icon-512x512.png?v=8',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: '/splash-icon.jpg?v=4',
+        src: '/splash-icon.jpg?v=5',
         sizes: '512x512',
         type: 'image/jpeg',
         purpose: 'any',
