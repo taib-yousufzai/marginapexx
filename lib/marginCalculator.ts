8bb2dbcef59ceee3cebd0bbdf86e9fee570f63ca
taib-yousufzai <kinglucky687@gmail.com>
2026-07-02 20:41:03 +0530

    chore: revert option margin hardcode to use dynamic backend settings

--- lib/marginCalculator.ts
+++ lib/marginCalculator.ts
@@ -24,8 +24,8 @@ export function calculateMarginPortion({
   // Options (both Buy and Sell) must always use fixed template margins, irrespective of leverageType setting.
   // It should not depend on dynamic variables like baseExposure (which uses premium/LTP).
   if (isOption) {
-    // Temporary hardcode: ALL options (Buy and Sell) use exactly 10,000 as margin per lot
-    return (totalQty / lotSize) * 10000;
+    // Uses the fixed margin value (leverage) configured in the admin panel
+    return (totalQty / lotSize) * leverage;
   } else if (leverageType === 'Fixed') {
     return (totalQty / lotSize) * leverage;
   } else if (leverageType === '%') {
